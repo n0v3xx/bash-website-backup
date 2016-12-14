c9d4274070ba263d10c78b392561f31325a30859
m.reschke <manuel.reschke90@gmail.com>
2016-12-14 14:15:09 +0100

    Fix something

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # bash-website-backup
 Simple bash script to backup a website directory
+
+## How to use?
+Modify the config in backup-website.sh to your needs!
+
+Then run:
+
+    chmod +x backup-website.sh
+    ./backup-website.sh
+
+### Backup to Dropbox
+If you want to use the dropbox upload. Install dropbox uploader (https://github.com/andreafabrizi/Dropbox-Uploader)
+
+    curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
+    
+Run uploader and follow install instructions.
+
+    chmod +x dropbox_uploader.sh
+    ./dropbox_uploader.sh
+
+Change dropbox uploader settings in backup-website.sh. Thats it.
+
+### Cronjob
+If you want the backup every day? Use crontab.
+
+    crontab -e
+    # Add the following line
+    0 1 * * * /path/to/script/backup-website.sh &> /dev/null
--- backup-website.sh
+++ backup-website.sh
@@ -5,24 +5,31 @@
 ######################################################################################################
 
 # Modify me #
-BACKUP_MODE=1 # 1 = local | 2 = local & dropbox | 3 = dropbox
-PATH_BACKUP="/root/backup" # Full path to the backup directory
-PATH_TARGET="/var/www/mysite"   # Full path to the target
-PATH_EXCLUDE="/var/www/mysite/images" # Full path to excluded folder
-FILENAME="backup-website" # archive name
-DATE=$(date +"%Y-%m-%d")  # example 2016-12-08
+# Full path to the backup directory
+PATH_BACKUP="/path/to/bash-website-backup/backup"
+# Full path to the target
+PATH_TARGET="/path/to/www/mysite"
+# Full path to excluded folder, you can use multiple folders
+PATH_EXCLUDE=("/path/to/test/images" "/path/to/test/tmp")
+# archive name
+FILENAME="backup-website"
+# enable the backup delete after x days
+BACKUP_DELETE_ENABLE=1
+# how many days you store a backup
+BACKUP_DELETE_DAYS=10
 
 # dropbox upload
 # https://github.com/andreafabrizi/Dropbox-Uploader
-DROPBOX_ENABLE=1                                   # 1 = On / 0 = Off
+DROPBOX_ENABLE=0                                   # 1 = On / 0 = Off
 DROPBOX_PATH_CONFIG="/root/.dropbox_uploader"
 DROPBOX_PATH_UPLOADER="/root/dropbox_uploader.sh"
 DROPBOX_PATH_FOLDER="/backup"                      # backup folder on dropbox
 DROPBOX_DELETE_DATE=$(date --date="-1 day" +"%Y-%m-%d")
 
 # Dont touch me #
-SCRIPT_NAME="Website-Backup:"
+SCRIPT_NAME="+Website-Backup:"
 PATH_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATE=$(date +"%Y-%m-%d")
 
 ######################################################################################################
 ############## Script Logic Starts here ##############################################################
@@ -32,6 +39,10 @@ PATH_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # https://www.cyberciti.biz/tips/understanding-linux-unix-umask-value-usage.html
 # Owner: 0 = read, write and execute | Group: 7 = no permissions | Others: 7 = no permissions
 umask 177
+
+#######################################
+# Check Paths #########################
+#######################################
 
 if ! [ -d "$PATH_BACKUP" ]
     then
@@ -50,39 +61,55 @@ if ! [ -d "$PATH_EXCLUDE" ]
         echo "${SCRIPT_NAME} No exclude path"
         exclude=0
     else
-        echo "${SCRIPT_NAME} Exclude path: ${PATH_EXCLUDE}"
+        exclude_string=''
+        for i in ${PATH_EXCLUDE[@]}; do
+            exclude_string="${exclude_string} --exclude='${i}'"
+            echo "${SCRIPT_NAME} Exclude path: ${i}"
+        done
+        #echo "${SCRIPT_NAME} Exclude path: ${PATH_EXCLUDE}"
         exclude=1
 fi
+
+#######################################
+# Backup Local ########################
+#######################################
 
 if test ${exclude} == 1
     echo "${SCRIPT_NAME} Start Backup"
     then
         # packen
         # https://wiki.ubuntuusers.de/Skripte/Backupscript/
-        tar --exclude="${PATH_EXCLUDE}" -zcf ${PATH_BACKUP}/${FILENAME}-${DATE}.tar.gz ${PATH_TARGET}
+        backup="tar ${exclude_string} -zcf ${PATH_BACKUP}/${FILENAME}-${DATE}.tar.gz ${PATH_TARGET}"
+        eval ${backup} # execute with eval becasue bash sucks
     else
         tar -zcf ${PATH_BACKUP}/${FILENAME}-${DATE}.tar.gz ${PATH_TARGET}
 fi
 echo "${SCRIPT_NAME} Backup Finish"
 
-# Delete files older than 10 days (only .tar.gz files)
-echo "${SCRIPT_NAME} Delete old .tar.gz files"
-find ${PATH_BACKUP}/* -name '*.sql.gz' -mtime +10 -exec rm {} \;
+if delete ${BACKUP_DELETE_ENABLE} == 1
+    then
+        # Delete files older than xy days (only .tar.gz files)
+        echo "${SCRIPT_NAME} Delete old .tar.gz files"
+        delete_command="find ${PATH_BACKUP}/* -name '*.tar.gz' -mtime +${BACKUP_DELETE_DAYS} -exec rm {} \;"
+        eval ${delete_command}
+    else
+        echo "${SCRIPT_NAME} Delete is not active"
+fi
 
-# check dropbox upload
+
+#######################################
+# Dropbox Upload ######################
+#######################################
+
 if test ${DROPBOX_ENABLE} == 1
     then
         # upload to dropbox
         bash ${DROPBOX_PATH_UPLOADER} -f ${DROPBOX_PATH_CONFIG} upload ${PATH_BACKUP}/${FILENAME}-${DATE}.tar.gz ${DROPBOX_PATH_FOLDER}
         # delete old uploads
-        echo "Try to delete "
+        echo "${SCRIPT_NAME} Try to delete "
         bash ${DROPBOX_PATH_UPLOADER} -f ${DROPBOX_PATH_CONFIG} delete ${DROPBOX_PATH_FOLDER}/${FILENAME}-${DROPBOX_DELETE_DATE}.tar.gz
     else
-        echo "Dropbox upload not enabled"
+        echo "${SCRIPT_NAME} Dropbox upload not enabled"
 fi
 
 echo "${SCRIPT_NAME} Done"
-
-
-# entpacken
-#tar -zxvf archiv.tar.gz
